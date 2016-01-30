601df2acc769cbfba9f3f38f57cb4d7489186085
Lixin Wang <Lwang401@gatech.edu>
2016-01-30 11:10:33 -0500

    testing git commands using terminal

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,7 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
+	  
 	  String returnString = "";
 	  String front = input.substring(2, input.length());
 	  String back = input.substring(0, 2);
